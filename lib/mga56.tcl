3ede2f0d7516cc0af8fd51511aea7322ace5c121
pdean <p.dean@internode.on.net>
2023-07-01 14:13:46 +1000

    mga screen format

--- lib/mga56.tcl
+++ lib/mga56.tcl
@@ -28,8 +28,8 @@ oo::objdefine mga56 {
             if {$mode >=2} {
                 if {![info exists track]} { set track 0.0 }
                 lassign [proj_fwd $P [list $lat $lon]] east north
-                set l1 [format "E%8.0f" $east]
-                set l2 [format "N%8.0f" $north]
+                set l1 [format "E%9.1f" $east]
+                set l2 [format "N%9.1f" $north]
                 set l3 "hdop: $hdop"
                 set l4 "$speed m/s [compass $track]"
                 lcd putlines $scr [list $l1 $l2 $l3 $l4]
