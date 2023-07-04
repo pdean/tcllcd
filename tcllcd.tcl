4bc2f4ca4211aa9abae95bc422ad6451b4e2f6f4
peter@arch1701 <p.dean@internode.on.net>
2023-07-04 11:06:13 +1000

    tidy tcllcd

--- tcllcd.tcl
+++ tcllcd.tcl
@@ -4,31 +4,22 @@
 lappend auto_path lib
 package require task
 package require lcd
-
 package require led
 Led create led1
 Led create led2
 Led create led3
 
 # database
-
 package require tdbc::postgres
 set conninfo [list -host localhost -db gis -user gis]
 tdbc::postgres::connection create db {*}$conninfo
 
-
 # define screens
-#set screens [list date  uptime gps]
-#set screens [list datetime uptime gps tmr]
-#set screens [list datetime host uptime gps mga56 tmr]
-#set screens [list datetime host uptime gps mga56 tmr nav]
-
+# set screens [list datetime host uptime gps mga56 tmr nav]
 package require fileutil
-
 set datadir /mnt/usb
 set screens [split [fileutil::cat [file join $datadir screens.txt]]]
 puts $screens
-
 proc definescreens {} {
     foreach scr $::screens {
         if {[string length $scr]} {
@@ -52,7 +43,6 @@ proc ignore {} {
 proc updatescreen {} {
       catch { [lcd screen] updatescreen } 
 }
-
 proc init {} {
     lcd connect
     task init
